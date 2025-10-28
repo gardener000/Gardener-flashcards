ac7a82b940481bf13951f28c6a9f88c78cc1e74c
unknown <330110650@qq.com>
2025-10-28 10:26:12 +0800

    change the menu to have a proper order

--- data/sets.js
+++ data/sets.js
@@ -1,68 +1,76 @@
 // data/sets.js
 export const cardSetsConfig = {
-  "threads": {
-    name: "操作系统：线程",
-    path: "/data/threads.js"
-  },
+  //java
   "java":{
     name:"Java：StringBuilder",
     path:"/data/java.js"
-  },
-  "Java":{
-    name:"Java:week3作业",
-    path:"/data/javaw3.js"
   },
   "java1":{
     name:"Java：接口、枚举与继承",
     path:"/data/java1.js"
   },
-  "toc":{
-    name:"toc_lec3:DFA&NFA",
-    path:"/data/toc_lec3.js"
+  "java-w2ppt":{
+    name:"Java: 2 Basic Java",
+    path:"/data/java-w2-review.js"
   },
-  "aai":{
-    name:"aai_chap2：微处理器体系结构",
-    path:"/data/microprocessor-architecture.js"
-  },
-  "na1":{
-    name:"数值分析：线性系统迭代法 (上)",
-    path:"/data/chap7_part1.js"
-  },
-  "cnMAC-1":{
-    name:"计算机网络：MAC层（上）",
-    path:"/data/mac-layer-part1.js"
-  },
-  "os-chap5":{
-    name:"操作系统：CPU调度算法",
-    path:"/data/cpu_scheduling.js"
+  "Java":{
+    name:"Java:week3作业",
+    path:"/data/javaw3.js"
   },
   "javaw6hw":{
     name:"Java 核心容器：数组与集合W6作业",
     path:"/data/java-containers.js"
   },
+  "javaw7hw":{
+    name:"JavaW7作业：异常处理",
+    path:"/data/java-w7-hw.js"
+  },
+  //toc
+  "toc":{
+    name:"toc_lec3:DFA&NFA",
+    path:"/data/toc_lec3.js"
+  },
+  
+  //cn
+  "cnMAC-1":{
+    name:"计算机网络：MAC层（上）",
+    path:"/data/mac-layer-part1.js"
+  },
+  //os
+  "os-chap5":{
+    name:"操作系统：CPU调度算法",
+    path:"/data/cpu_scheduling.js"
+  },
+  
+  "threads": {
+    name: "操作系统：线程",
+    path: "/data/threads.js"
+  },
+  //na
+  "na1":{
+    name:"数值分析：线性系统迭代法 (上)",
+    path:"/data/chap7_part1.js"
+  },
   "na2":{
     name:"数值分析：迭代法",
     path:"/data/na2.js"
   },
-  "aai4-1":{
-    name:"aai_chap4-1：数据移动与编码",
-    path:"/data/assembly_chap4.js"
-  },
-  "java-w2ppt":{
-    name:"Java: 2 Basic Java",
-    path:"/data/java-w2-review.js"
+  //aai
+  "aai":{
+    name:"aai_chap2：微处理器体系结构",
+    path:"/data/microprocessor-architecture.js"
   },
   "aai-chap3-1":{
-    name:"汇编Chap3:寻址方式（上）",
+    name:"aai_chap3:寻址方式（上）",
     path:"/data/aai_chap3_1.js"
   },
   "aai-chap3-2":{
-    name:"汇编Chap3:寻址方式（下）",
+    name:"aai_chap3:寻址方式（下）",
     path:"/data/aai_chap3_2.js"
   },
-  "javaw7hw":{
-    name:"JavaW7作业：异常处理",
-    path:"/data/java-w7-hw.js"
+  "aai4-1":{
+    name:"aai_chap4-1：数据移动与编码",
+    path:"/data/assembly_chap4.js"
   }
 
   
